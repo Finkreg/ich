e4d8d4e1584ca8d051295027cde3e7eb5f3c3a4a
Oleksii Starodubov <133102040+Finkreg@users.noreply.github.com>
2025-04-10 23:44:09 +0200

    Update hw_36.py

--- python/hw_36.py
+++ python/hw_36.py
@@ -3,20 +3,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-# def print_links(url = "https://www.wikipedia.org/"):
-#     response = requests.get(url)  #с помощью библиотеки requests обрабатываем полученные данные
-#     soup = BeautifulSoup(response.text, 'html.parser') #Варим суп :) на основе HTML текста
-#     for link in soup.find_all('a'): #используем цикл и метод find_all чтобы найти все гиперссылки
-#         href = link.get('href')
-#         if href.startswith('//'): #обрабатываем ссылки которые начинаются с // чтобы в консоли они начинались с https и правильно отображались
-#             href = 'https:' + href
-#             print(href)  # Выводим ссылку
+def print_links(url = "https://www.wikipedia.org/"):
+    response = requests.get(url)  #с помощью библиотеки requests обрабатываем полученные данные
+    soup = BeautifulSoup(response.text, 'html.parser') #Варим суп :) на основе HTML текста
+    for link in soup.find_all('a'): #используем цикл и метод find_all чтобы найти все гиперссылки
+        href = link.get('href')
+        if href.startswith('//'): #обрабатываем ссылки которые начинаются с // чтобы в консоли они начинались с https и правильно отображались
+            href = 'https:' + href
+            print(href)  # Выводим ссылку
 
         
 
-# # webpage = input("enter the URL: ") # просим юзера ввести вдрем вебсайта
-# # print_links(webpage)
-# print_links()
+# webpage = input("enter the URL: ") # просим юзера ввести вдрем вебсайта
+# print_links(webpage)
+print_links()
 
 
 #     Напишите программу, которая запрашивает у пользователя URL-адрес веб-страницы и уровень заголовков, а 
@@ -34,3 +34,7 @@ def select_headers(url, header_lvl = 'h1'):
 
 link = "https://www.crummy.com/software/BeautifulSoup/bs4/doc/"
 select_headers(link)
+
+# link = input(f"enter the URL: ")
+# header = input(f"enter the header level: ")
+# select_headers(link, header)
