a461d122da7eb38cd498cda73fb0368781c21555
finkreg <finkreg@gmail.com>
2025-05-16 15:10:03 +0200

    added auto qa hw 1

--- python_advanced/flask/main.py
+++ python_advanced/flask/main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello Flask"
+
+@app.route("/handle_url_params")
+def handle_params():
+    if "greeting" in request.args.keys() and "name" in request.args.keys():
+        greeting = request.args["greeting"]
+        name = request.args.get("name")
+        return f"{greeting}, {name}"
+    else:
+        return "Some parameters are missing"
 
 
 @app.route("/user/<name>")
@@ -13,3 +22,4 @@ def greet(name):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
